da07207eccda0a05ae1e6b44da902993ade43b0b
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2024-03-05 16:07:53 -0500

    Initial Commit

---
+++ WeatherMate/WeatherMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMateApp.swift
+//  WeatherMate
+//
+//  Created by aniket adhikari on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
